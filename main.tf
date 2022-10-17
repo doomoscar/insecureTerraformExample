91afe52b821c69beed0554b215b0220385d24895
Oscar Carrion Artavia <oscarcarrionartavia@cloud.upwork.com>
2022-10-17 16:34:24 -0600

    Added Insecure code for example

--- main.tf
+++ main.tf
@@ -18,3 +18,14 @@ resource "azurerm_managed_disk" "source" {
         enabled = false
     }
 }
+
+ resource "aws_apigatewayv2_stage" "bad_example" {
+   api_id = aws_apigatewayv2_api.example.id
+   name   = "example-stage"
+ }
+
+ resource "aws_apigatewayv2_stage" "bad_example" {
+   deployment_id = aws_api_gateway_deployment.example.id
+   rest_api_id   = aws_api_gateway_rest_api.example.id
+   stage_name    = "example"
+ }
