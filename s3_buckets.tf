67be498f2fcb1c852c2329b4a246fa47520b33a4
Oscar Carrion Artavia <oscarcarrionartavia@cloud.upwork.com>
2022-11-22 11:01:16 -0600

    Create new file

---
+++ s3_buckets.tf
@@ -0,0 +1,20 @@
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.test.arn
+  port = 80
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "80"
+      protocol    = "HTTP"
+      status_code = "HTTP_301"
+    }
+  }
+}
+
+resource "aws_lb" "test" {
+  name = "test123"
+  load_balancer_type = "application"
+  subnets = [aws_subnet.subnet1.id, aws_subnet.subnet2.id]
+  internal = true
+}
