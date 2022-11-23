fb631c9df87423befb82cd8438a71c86220c49c4
Oscar Carrion Artavia <oscarcarrionartavia@cloud.upwork.com>
2022-11-23 10:07:34 -0600

    Adding EC2 code + private remote test

---
+++ EC2.tf
@@ -0,0 +1,16 @@
+resource "aws_instance" "bad_example" {
+  ami = "ami-7f89a64f"
+  instance_type = "t1.micro"
+
+  root_block_device {
+      encrypted = false
+  }
+
+  ebs_block_device {
+    device_name = "/dev/sdg"
+    volume_size = 5
+    volume_type = "gp2"
+    delete_on_termination = false
+    encrypted = false
+  }
+}
