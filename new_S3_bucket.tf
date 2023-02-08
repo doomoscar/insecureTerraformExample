d30857669f62cbe1ea913e071d911ff9b0c3ae1b
Oscar Carrion <36800391+doomoscar@users.noreply.github.com>
2023-02-07 23:55:31 -0600

    Secure S3 buckets

--- new_S3_bucket.tf
+++ new_S3_bucket.tf
@@ -2,11 +2,27 @@ resource "aws_s3_bucket" "example" {
   bucket = "mybucket"
 }
 
-resource "aws_s3_bucket_public_access_block" "bad_example" {
-  bucket = aws_s3_bucket.example.id
-}
-
-resource "aws_s3_bucket_public_access_block" "bad_example" {
+resource "aws_s3_bucket_public_access_block" "good_example" {
   bucket = aws_s3_bucket.example.id 
-  block_public_policy = false
+  block_public_policy = true
+  block_public_acls = true
+  versioning {
+        enabled = true
+    }
+  
+  server_side_encryption_configuration {
+     rule {
+       apply_server_side_encryption_by_default {
+         kms_master_key_id = "arn"
+         sse_algorithm     = "aws:kms"
+       }
+     }
+   }
+  
+  logging {
+        target_bucket = "target-bucket"
+    }
+  
+  restrict_public_buckets = true
+  
 }
