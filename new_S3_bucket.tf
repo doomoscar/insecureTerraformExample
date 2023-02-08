7093da4498214e76159782cf041777dbace3c6eb
Oscar Carrion <36800391+doomoscar@users.noreply.github.com>
2023-02-08 00:11:30 -0600

    Secure new S3 bucket

--- new_S3_bucket.tf
+++ new_S3_bucket.tf
@@ -1,11 +1,6 @@
 resource "aws_s3_bucket" "example" {
   bucket = "mybucket"
-}
-
-resource "aws_s3_bucket_public_access_block" "good_example" {
-  bucket = aws_s3_bucket.example.id 
-  block_public_policy = true
-  block_public_acls = true
+  ignore_public_acls = true
   versioning {
         enabled = true
     }
@@ -24,5 +19,30 @@ resource "aws_s3_bucket_public_access_block" "good_example" {
     }
   
   restrict_public_buckets = true
-  
+}
+
+resource "aws_s3_bucket_public_access_block" "good_example" {
+  bucket = aws_s3_bucket.example.id 
+  block_public_policy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "good_example" {
+  bucket = aws_s3_bucket.example.id
+  block_public_acls = true
+}
+
+resource "aws_s3_bucket_object_lock_configuration" "example" {
+  bucket = aws_s3_bucket.example.bucket
+  object_lock_enabled = Enabled
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.example.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
 }
