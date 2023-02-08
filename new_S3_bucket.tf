48911687debca18962b5284ca79e64fadc136ca3
Oscar Carrion <36800391+doomoscar@users.noreply.github.com>
2023-02-07 23:37:02 -0600

    Update new_S3_bucket.tf

--- new_S3_bucket.tf
+++ new_S3_bucket.tf
@@ -1,27 +1,12 @@
-resource "aws_cloudfront_distribution" "positive2" {
-  origin {
-    domain_name = aws_s3_bucket.b.bucket_regional_domain_name
-    origin_id   = local.s3_origin_id
+resource "aws_s3_bucket" "example" {
+  bucket = "mybucket"
+}
 
-    s3_origin_config {
-      origin_access_identity = "origin-access-identity/cloudfront/ABCDEFG1234567"
-    }
-  }
+resource "aws_s3_bucket_public_access_block" "bad_example" {
+  bucket = aws_s3_bucket.example.id
+}
 
-  enabled             = true
-  comment             = "Some comment"
-  default_root_object = "index.html"
-
-  default_cache_behavior {
-    #settings
-  }
-
-  restrictions {
-    #restrictions
-  }
-
-  viewer_certificate {
-    cloudfront_default_certificate = false
-    minimum_protocol_version = "TLSv1_2016"
-  }
+resource "aws_s3_bucket_public_access_block" "bad_example" {
+  bucket = aws_s3_bucket.example.id 
+  block_public_policy = false
 }
