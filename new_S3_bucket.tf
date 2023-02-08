1f9c175e59ce731f22cbb3df2c6b6c4f88121985
Oscar Carrion Artavia <oscarcarrionartavia@cloud.upwork.com>
2023-02-08 09:38:52 -0600

    Increased S3 security

--- new_S3_bucket.tf
+++ new_S3_bucket.tf
@@ -153,10 +153,24 @@ resource "aws_s3_bucket" "example" {
     }
   }
 
+  resource "aws_s3_bucket_public_access_block" "example" {
+    bucket = aws_s3_bucket.example.id
+
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
+  }
+  resource "aws_kms_key" "mykey" {
+    description             = "This key is used to encrypt bucket objects"
+    deletion_window_in_days = 10
+  }
+
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
+        kms_master_key_id = aws_kms_key.mykey.arn
+        sse_algorithm = "aws:kms"
       }
     }
   }
